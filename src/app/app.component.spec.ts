43e43ba1046169358d25621bb7e9b46b46d94c46
jesusriv <r.jesus9107@gmail.com>
2019-11-08 07:16:02 -0800

    app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'public'`, () => {
+  it(`should have as title 'image-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('public');
+    expect(app.title).toEqual('image-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('public app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('image-app app is running!');
   });
 });
